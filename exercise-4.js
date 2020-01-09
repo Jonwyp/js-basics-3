68ca996dbe7f3026e18a6bb5a4cff8790391e356
Jonathan Wong <Jonwyp@users.noreply.github.com>
2020-01-09 17:46:43 +0800

    Add my workings to exercise 4 and 5

--- exercise-4.js
+++ exercise-4.js
@@ -1,31 +1,34 @@
 // 1. Create an array `names` with at least 3 elements: `'david'`, `'gordon'`, and
 //    `'sahil'` and log it to the console
-
-
+const names = ["david", "gordon", "sahil"];
+console.log(names);
 // 2. Log the value of the third element in `names` to the console
-
-
+const thirdName = names[2];
+console.log(thirdName);
 // 3. Log the value of the `names` array's `length` property to the console
-
-
+let nameLength = names.length;
+console.log(nameLength);
 // 4. Set the value of the `length` property to `1`
-
-
+nameLength = 1;
+console.log(nameLength);
 // 6. Once again, log the value of the `names` array's `length` property to the
 //    console. Did it change?
-
+"Yes it did"
 
 // 7. Log the entire `names` array to the console again
-
+console.log(names);
 
 // 8. Add a few more elements to the array using `push`
-
-
+names.push("john", "ben", "adam");
+console.log(names);
 // 9. Log the last element of the array to the console
-
-
+const lastName = names.length - 1;
+console.log(lastName);
+console.log(names[lastName]);
 // 10. Log the second last element of the array to the console
-
+const secondLastName = names.length - 2;
+console.log(secondLastName);
+console.log(names[secondLastName]);
 
 // 11. Change the `textContent` property of the _second_ `p` tag on the page to
 //    `"JavaScript was here!"`
@@ -34,8 +37,8 @@
 // 12. Convert the `names` array to a string, with each element separated by a
 //    '<br>'. Store it in a new variable called `namesString` e.g. if `names` is
 //    `['harry', 'ron', 'dean']` then `namesString` will be `"harry<br>ron<br>dean"`
-
-
+const element = names.join("<br>");
+console.log(element);
 // 13. Change the `innerHTML` property of the _first_ `p` tag on the page to
 //    `namesString`. What happens when you use `textContent` instead of
 //    `innerHTML`?
--- exercise-5.js
+++ exercise-5.js
@@ -1,17 +1,34 @@
 // 1. Create a new array `shoppingList` with a few string elements
-
-
-// 2. Using `forEach`, log every element of the `shoppingList` array to the console
-
-
+const shoppingList = ["table", "chair","cabinet"];
+console.log(shoppingList);
+// 2a. Using for loop to log every element
+for (let i = 0; i < shoppingList.length; i++) {
+    console.log(shoppingList[i]);
+}; 
+// 2b. Using `forEach`, log every element of the `shoppingList` array to the console
+shoppingList.forEach(i => console.log(i));
 // 3. For each element in the `shoppingList` array, create an `<li>` node and
 //    append it to the `<ul>` element on the page (use `document.createElement`
 //    and `.appendChild`)
 
 
 // 4. Create a new array `numbers` with a few numeric elements
-
-
-// 5. Using `map` on the `numbers` array, create a new array called
+const numbers = [1, 2, 3, 4, 5];
+console.log(numbers);
+// 5a. Using `for` on the `numbers` array, create a new array called
 //    `squareNumbers` where each element is the square of the corresponding
 //    element in the `numbers` array
+
+let newNumbers = []; 
+for (let i = 0; i < numbers.length; i++) {
+    newNumbers.push(numbers[i]**2);
+}
+console.log(newNumbers);
+// 5b. Using `map` on the `numbers` array, create a new array called
+//    `squareNumbers` where each element is the square of the corresponding
+//    element in the `numbers` array
+const squareNumbers = numbers.map(number => number ** 2);
+// const squareNumbers = numbers.map(function(number){
+//     return number ** 2;
+// };
+console.log(squareNumbers);
